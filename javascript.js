89b1da4227c22afff85c4fd09791749ad1c77c05
Nathaly Alves <nathalyalves0209@gmail.com>
2025-04-06 21:47:09 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,83 +1,87 @@
-prompt("Ímpar ou Par?");
-escolhaJogador = prompt("Esolha um número entre 1 e 5.");
-escolhaComputador = Math.floor(Math.random() * 5) + 1;
-if (escolhaJogador == escolhaComputador) {
-  alert("PAR GANHOU");
-}
+function conversor() {
+  prompt("Ímpar ou Par?");
+  escolhaJogador = prompt("Esolha um número entre 1 e 5.");
+  escolhaComputador = Math.floor(Math.random() * 5) + 1;
+  if (escolhaJogador == escolhaComputador) {
+    alert("PAR GANHOU");
+  }
 
 
-if (escolhaJogador == 1) {
-  if (escolhaComputador == 2) {
-    alert("ÍMPAR GANHOU");
+
+  if (escolhaJogador == 1) {
+    if (escolhaComputador == 2) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 3) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 4) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 5) {
+      alert("PAR GANHOU");
+    }
   }
-  if (escolhaComputador == 3) {
-    alert("PAR GANHOU");
+
+  if (escolhaJogador == 2) {
+    if (escolhaComputador == 1) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 3) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 4) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 5) {
+      alert("ÍMPAR GANHOU");
+    }
   }
-  if (escolhaComputador == 4) {
-    alert("ÍMPAR GANHOU");
+
+  if (escolhaJogador == 3) {
+    if (escolhaComputador == 1) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 2) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 4) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 5) {
+      alert("PAR GANHOU");
+    }
   }
-  if(escolhaComputador == 5) {
-    alert("PAR GANHOU");
+
+  if (escolhaJogador == 4) {
+    if (escolhaComputador == 1) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 2) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 3) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 5) {
+      alert("ÍMPAR GANHOU");
+    }
   }
+  if (escolhaJogador == 5) {
+    if (escolhaComputador == 1) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 2) {
+      alert("ÍMPAR GANHOU");
+    }
+    if (escolhaComputador == 3) {
+      alert("PAR GANHOU");
+    }
+    if (escolhaComputador == 4) {
+      alert("ÍMPAR GANHOU");
+    }
+  }
+  alert("A escolha do computador foi: " + escolhaComputador);
 }
 
-if (escolhaJogador == 2) {
-  if (escolhaComputador == 1) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 3) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 4) {
-    alert("PAR GANHOU");
-  }
-  if (escolhaComputador == 5) {
-    alert("ÍMPAR GANHOU");
-  }
-}
-
-if (escolhaJogador == 3) {
-  if (escolhaComputador == 1) {
-    alert("PAR GANHOU");
-  }
-  if (escolhaComputador == 2) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 4) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 5) {
-    alert("PAR GANHOU");
-  }
-}
-
-if (escolhaJogador == 4) {
-  if (escolhaComputador == 1) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 2) {
-    alert("PAR GANHOU");
-  }
-  if (escolhaComputador == 3) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 5) {
-    alert("ÍMPAR GANHOU");
-  }
-}
-if (escolhaJogador == 5) {
-  if (escolhaComputador == 1) {
-    alert("PAR GANHOU");
-  }
-  if (escolhaComputador == 2) {
-    alert("ÍMPAR GANHOU");
-  }
-  if (escolhaComputador == 3) {
-    alert("PAR GANHOU");
-  }
-  if (escolhaComputador == 4) {
-    alert("ÍMPAR GANHOU");
-  }
-}
-alert("A escolha do computador foi: " + escolhaComputador);
 
